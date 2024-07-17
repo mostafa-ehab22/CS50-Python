80b4548c30a1c49cc5a16a4d9ec6cd5452c2eaae
Mostafa Ehab <mostafaehab0002@gmail.com>
2024-07-18 02:12:04 +0300

    Added lines.py

---
+++ Week 6 - File I_O/lines.py
@@ -0,0 +1,40 @@
+"""
+Program counts file's lines excluding:
+    Blank lines
+    docstrings
+"""
+import sys
+
+def main():
+    # Command-line argv != 2
+    if len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    elif len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+
+    # Command-line argv == 2
+    else:
+        entry = sys.argv[1]
+        lines_count = 0
+
+        # Not Python file
+        if not entry.endswith(".py"):
+            sys.exit("Not a Python file")
+
+        # Count lines of file
+        try:
+            with open(entry) as file:
+                for line in file:
+                    stripped_line = line.strip()
+                    if stripped_line and not stripped_line.startswith("#"):
+                        lines_count += 1
+            print(f"Lines count: {lines_count}")
+        except FileNotFoundError:
+            sys.exit("File does not exist")
+        except Exception as e:
+            sys.exit(f"An error occured: {e}")
+
+
+
+if __name__ == "__main__":
+    main()

e94548bc553081bcebd3d0e7b89a718fd6a7aa16
Mostafa Ehab <mostafaehab0002@gmail.com>
2024-07-18 13:31:53 +0300

    Added pizza.py

---
+++ Week 6 - Files/pizza.py
@@ -0,0 +1,30 @@
+from tabulate import tabulate
+import sys
+import csv
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+
+    else:
+        filename = sys.argv[1]
+
+        if not filename.endswith(".csv"):
+            sys.exit("Not a CSV file")
+
+        try:
+            with open(filename) as file:
+                    reader = csv.reader(file)
+                    rows = [row for row in reader]
+
+            print(tabulate(rows,headers="firstrow",tablefmt="grid"))
+
+        except FileNotFoundError:
+            sys.exit("File does not exist")
+
+
+if __name__ == "__main__":
+    main()
+

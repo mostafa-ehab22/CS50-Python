dc2021b066143e57c9b39911e871aeb36bc2e155
Mostafa Ehab <mostafaehab0002@gmail.com>
2024-07-20 05:05:41 +0300

    Modified emojize.py

--- Week 4 - Libraries/emojize.py
+++ Week 4 - Libraries/emojize.py
@@ -2,7 +2,7 @@ import emoji
 
 def main():
     user_input = input("Input: ")
-    print(f"Output: {emoji.emojize(user_input, language="alias")}")
+    print(f"Output: {emoji.emojize(user_input, language='alias')}")
 
 
 main()
